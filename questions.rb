b96a058e37cf2255b9dabff04829e7106145a9aa
Paige Nelmes <designer@paigenelmes.com>
2023-03-09 11:46:37 -0800

    Add code for Questions class

--- questions.rb
+++ questions.rb
@@ -1,5 +1,20 @@
-# Generate a random number between 1 - 20
+class Question
+  attr_accessor :question, :solution
 
-# Generate a random operator (+, -, * /)
+  # Generate 2 random numbers between 1 - 20
+  randomNum1 = rand(1...20)
+  randomNum2 = rand(1...20)
 
-# Put these together to create a random question
+  # Generate a random operator (+, -, * /)
+  operators = [:+, :-, :*, :/]
+  randomOp = operators.sample
+
+  # Put these together to create a random question
+  @question = "What is #{randomNum1} #{randomOp} #{randomNum2}?"
+  puts @question
+
+  # Get the solution
+  @solution = randomNum1.method(randomOp).(randomNum2)
+  puts @solution
+ 
+end
